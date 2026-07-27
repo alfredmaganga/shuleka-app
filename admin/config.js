6b0bea8ddf623627493ba94958ee5f4068a95512
alfredmaganga <alfredmaganga@users.noreply.github.com>
2026-07-27 20:59:31 +0000

    deploy: f8738aea5c56ef1b3bcb38101bd738bc31bbecec

--- admin/config.js
+++ admin/config.js
@@ -3,3 +3,4 @@ const SUPABASE_URL = 'https://uiwgbviucbbqcxdkpdwa.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpd2didml1Y2JicWN4ZGtwZHdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxNzI4MjIsImV4cCI6MjEwMDc0ODgyMn0.FN3z7fr4sbkMCIlTq_pXxccz0a-kqBUXghksoFYKJWg';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+console.log('Supabase client initialized');
